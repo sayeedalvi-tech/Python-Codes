f93cffc7967bec992bfb5619fed0cd21cba3ea80
sayeeeee21 <sayeedalvi384@gmail.com>
2026-02-06 15:21:56 +0530

    Python

--- Loops/if_elif_06FEB.py
+++ Loops/if_elif_06FEB.py
@@ -1,27 +1,27 @@
-# #1. WaP to check if the given number is postive or negative or neutral
-# a=int(input("Enter Number:"))
-# if a > 0:
-#     print("Positive")
-# elif a < 0:
-#     print("Negative")
-# else:
-#     print("Neutral")
+#1. WaP to check if the given number is postive or negative or neutral
+a=int(input("Enter Number:"))
+if a > 0:
+    print("Positive")
+elif a < 0:
+    print("Negative")
+else:
+    print("Neutral")
 
-# #2. Wap to check if given character is upper case or lower case or digit
-# a=eval(input("Enter String"))
-# if a.isupper():
-#     print("Upper case")
-# elif a.islower():
-#     print("Lower Case")
-# elif a.isdigit():
-#     print("Digit")
+#2. Wap to check if given character is upper case or lower case or digit
+a=eval(input("Enter String"))
+if a.isupper():
+    print("Upper case")
+elif a.islower():
+    print("Lower Case")
+elif a.isdigit():
+    print("Digit")
 
-# if ord('A') >= ord(a) >= ord('Z'):
-#     print("Upper case")
-# elif ord('a') >= ord(a) >= ord('z'):
-#     print("Lower Case")
-# elif ord('0') >= ord(a) >= ord('9'):
-#     print("Digit")
+if ord('A') >= ord(a) >= ord('Z'):
+    print("Upper case")
+elif ord('a') >= ord(a) >= ord('z'):
+    print("Lower Case")
+elif ord('0') >= ord(a) >= ord('9'):
+    print("Digit")
 
 #3. WAP to check the given data type is string and return its length else given data type is list pop the element else
 # if tuple then reverse the tuple else invalid dattype
